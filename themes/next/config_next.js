d6fa323ce5b52f059b73abeecb1a14f2e30dc312
SuperHuangXu <www.1567891@qq.com>
2023-02-09 22:37:01 +0800

    feat: 🎸 修改主题配置

--- themes/next/config_next.js
+++ themes/next/config_next.js
@@ -5,15 +5,15 @@ const CONFIG_NEXT = {
 
   NAV_TYPE: 'normal', // ['fixed','autoCollapse','normal'] 分别是固定屏幕顶部并始终显示、固定屏幕顶部且滚动时隐藏，不固定屏幕顶部
 
-  POST_LIST_COVER: false, // 文章列表显示封面图
+  POST_LIST_COVER: true, // 文章列表显示封面图
   POST_LIST_PREVIEW: true, // 显示文章预览
-  POST_LIST_SUMMARY: false, // 显示用户自定义摘要，有预览时优先只展示预览
+  POST_LIST_SUMMARY: true, // 显示用户自定义摘要，有预览时优先只展示预览
 
   POST_HEADER_IMAGE_VISIBLE: false, // 文章详情页是否显示封面图
 
   // 右侧组件
-  RIGHT_BAR: true, // 是否显示右侧栏
-  RIGHT_LATEST_POSTS: true, // 右侧栏最新文章
+  RIGHT_BAR: false, // 是否显示右侧栏
+  RIGHT_LATEST_POSTS: false, // 右侧栏最新文章
   RIGHT_CATEGORY_LIST: true, // 右侧边栏文章分类列表
   RIGHT_TAG_LIST: true, // 右侧边栏标签分类列表
   RIGHT_AD: false, // 右侧广告
@@ -27,13 +27,12 @@ const CONFIG_NEXT = {
 
   WIDGET_TO_TOP: true, // 是否显示回顶
   WIDGET_TO_BOTTOM: false, // 显示回底
-  WIDGET_DARK_MODE: false, // 显示日间/夜间模式切换
+  WIDGET_DARK_MODE: true, // 显示日间/夜间模式切换
   WIDGET_TOC: true, // 移动端显示悬浮目录
 
   ARTICLE_SHARE: false, // 文章分享功能
   ARTICLE_RELATE_POSTS: true, // 相关文章推荐
   ARTICLE_COPYRIGHT: true // 文章版权声明
-
 }
 
 export default CONFIG_NEXT
